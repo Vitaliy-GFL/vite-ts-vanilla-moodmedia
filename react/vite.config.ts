8251d8db4dfbb6b24d791ef6d68cfb7e053e257c
Vitaliy Shevchenko <vitaliy.shevchenko@moodmedia.com>
2026-01-30 15:29:19 +0200

    add custom Vite plugin for full reload on mframe.json changes and adjust debugger.scss positioning

--- react/vite.config.ts
+++ react/vite.config.ts
@@ -1,13 +1,29 @@
-import { defineConfig } from 'vite';
+import { defineConfig, ViteDevServer } from 'vite';
 import react from '@vitejs/plugin-react';
 import legacy from '@vitejs/plugin-legacy';
 import { resolve } from 'path';
 
 import pck from './package.json';
 
+// A custom plugin to force a full reload for mframe.json changes
+function publicDirReload() {
+  return {
+    name: 'public-dir-reload',
+    configureServer(server: ViteDevServer) {
+      server.watcher.add('public/mframe.json'); // Watch mframe.json file
+
+      server.watcher.on('change', (path: string) => {
+        if (path === 'public/mframe.json') {
+          server.hot.send({ type: 'full-reload' }); // Send full-reload event
+        }
+      });
+    },
+  };
+}
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [legacy({ renderModernChunks: false, targets: ['chrome 50'] }), react()],
+  plugins: [legacy({ renderModernChunks: false, targets: ['chrome 50'] }), react(), publicDirReload()],
   define: {
     __APP_VERSION__: `"${pck.version}"`,
   },
